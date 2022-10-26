66e80940c28a42753b7525fe9bb454dc6159a76e
Kevin Choye <kevinchoye@Kevins-MacBook-Pro.local>
2022-10-26 14:56:15 -0400

    First Commit

--- discussion_8.py
+++ discussion_8.py
@@ -6,22 +6,32 @@ import unittest
 # search for the url in the University of Michgian wikipedia page (in the third pargraph of the intro)
 # HINT: You will have to add https://en.wikipedia.org to the URL retrieved using BeautifulSoup
 def getLink(soup):
-    
-    pass
+    olympics = soup.find('a', title = "List of American universities with Olympic medals")
+    url = "https://en.wikipedia.org" + olympics.get('href')
+    return url
 
 # Task 3: Get the details from the box titled "College/school founding". Get all the college/school names and the year they were
 # founded and organize the same into key-value pairs.
 def getAdmissionsInfo2019(soup):
-
-    pass
-
-
+    admDict = {}
+    admissions = soup.find('table', class_ = "toccolours")    
+    tables = admissions.find_all('tr')
+    for table in tables:
+        info = table.find_all('td')
+        schoolName = info[0].text
+        year = info[1].text
+        admDict[schoolName] = year.strip("\n")
+    admDict.pop("College/school")
+    print(admDict)
+    return admDict
 
 def main():
     # Task 1: Create a BeautifulSoup object and name it soup. Refer to discussion slides or lecture slides to complete this
 
     #### YOUR CODE HERE####
-
+    url = "https://en.wikipedia.org/wiki/University_of_Michigan"
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
     #Call the functions getLink(soup) and getAdmissionsInfo2019(soup) on your soup object.
     getLink(soup)
     getAdmissionsInfo2019(soup)
@@ -34,7 +44,9 @@ class TestAllMethods(unittest.TestCase):
         self.assertEqual(getLink(self.soup), 'https://en.wikipedia.org/wiki/List_of_American_universities_with_Olympic_medals')
 
     def test_admissions_info(self):
-        self.assertEqual(getAdmissionsInfo2019(self.soup), {'Engineering': '1854', 
+        self.assertEqual(getAdmissionsInfo2019(self.soup), {'Literature, Science, andthe Arts': '1841',
+                                                            'Medicine': '1850',
+                                                            'Engineering': '1854', 
                                                             'Law': '1859',
                                                             'Dentistry': '1875', 
                                                             'Pharmacy': '1876', 
